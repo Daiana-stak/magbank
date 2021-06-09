5eb2cfb8c7da22d31e0b9f0358ba0e8b9bb6c36e
Daiana Magali <dmagali008mkt@gmail.com>
2021-06-08 22:31:35 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from './components/Header';
+import Navbar from './components/Navbar';
+import "./App.scss"; 
 
-function App() {
-  return (
+
+const App = () => (
     <div className="App">
-     
+     <Navbar />
     </div>
   );
-}
 
 export default App;
