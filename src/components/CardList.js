bbb1b3f811bac5e4af602ee33c90937e1bebef66
Daiana Magali <dmagali008mkt@gmail.com>
2021-06-25 22:14:33 -0300

    configurando credicar

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import posts from './data/posts';
+import './CardList.scss';
+
+const CardList = ({ posts }) => (
+    <Container>
+     <Row>
+    
+     </Row>
+    </Container>
+);
+
+export default CardList;
