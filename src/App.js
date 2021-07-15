bfbe29b30eecf0220db4f743de3f356f170543e2
unknown <dmagali008mkt@gmail.com>
2021-07-15 20:48:25 -0300

    aula 16

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import CreditCard from './components/CreditCard';
+import CardList from './components/CardList';
+import CenteredButton from './components/CenteredButton';
 import "./App.scss"; 
 
+import posts from './components/data/posts';
 
-const App = () => (
+const App = () => {
+  return(
     <div className="App">
-     <Navbar />
+    <Navbar/>
+    <Hero/>
+
+    <CreditCard/>
+    <CardList posts={posts} />
+    <CenteredButton/>
+  
     </div>
-  );
+  )
+}
 
 export default App;
