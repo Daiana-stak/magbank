f453a1308e4367b21e974c63982b4af39364bec9
Daiana Schneider <dmagali008mkt@gmail.com>
2021-08-06 20:06:39 -0300

    aula 16

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,21 +3,21 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import './CardList.scss';
 
 const CardList = ({ posts }) => (
-  <Container>
-    <Row>
-      {posts&&
-        posts.map (([ id, image, title, excerpt, action ]) => (
-          <Col xs={12} lg={4} key={id}>
-            <Card className='mx-auto my-3'>
-              <Card.Img variant='top' src={image} />
-              <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>{excerpt}</Card.Text>
-                <Button variant='danger'>{action}</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+    <Container>
+      <Row>
+        {posts.map} {(([ id, image, title, excerpt, action ]) => (
+            <Col xs={12} lg={4} key={id}>
+              <Card className='mx-auto my-3'>
+                <Card.Img variant='top' src={image} />
+                <Card.Body>
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Text>{excerpt}</Card.Text>
+                  <Button variant='danger'>{action}</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+           ))}
+            return posts.map;
     </Row>
   </Container>
 );
